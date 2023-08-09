09f009104e231b26bbcdc3dfc3046cd3b56d9de0
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-08-09 14:26:54 +0300

    Implament articles component with carousel effect

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,42 +1,75 @@
+import "./Articles.scss";
+
 import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
-// import ExampleCarouselImage from "components/ExampleCarouselImage";
 
 import articalFirst from "../../assets/images/article-first.png";
 import articalsecond from "../../assets/images/article-second.png";
 import articalThird from "../../assets/images/article-third.png";
 
+import ButtonPress from "../Button/Button";
+
+const articalsData = [
+  {
+    imgUrl: articalFirst,
+    textTopic: "Tips, Constructor",
+    textTime: " - March 10, 2023",
+    title: "Your Dream Website with a User-Friendly Constructor",
+    description:
+      "Building a website can be a daunting task, especially for those who lack technical expertise.",
+  },
+
+  {
+    imgUrl: articalsecond,
+    textTopic: "Tips, Constructor",
+    textTime: " - March 10, 2023",
+    title: "How ME-Page Can Help You Build Your Business Online",
+    description:
+      "With customizable templates, user-friendly interfaces, and integrated e-commerce features...",
+  },
+
+  {
+    imgUrl: articalThird,
+    textTopic: "Tips, Constructor",
+    textTime: " - March 10, 2023",
+    title: "Maximizing the Potential of Online Store with ME-Page",
+    description:
+      "From creating a visually appealing website to integrating essential features such as social...",
+  },
+];
+
 function Articles() {
   return (
-    <section className="py-s">
+    <section className="articles py-s">
       <Container>
         <h3 className="title-h3 mb-2">Articles</h3>
         <h2 className="title-h2">Latest news from ME-Page team</h2>
         <Carousel>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="First slide" /> */}
-            <Carousel.Caption>
-              {/* <img src={articalFirst} className="d-block w-100"></img> */}
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="Second slide" /> */}
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="Third slide" /> */}
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {articalsData.map((artical) => {
+            return (
+              <Carousel.Item>
+                <img src={artical.imgUrl} className="d-block w-100" />
+                <Carousel.Caption>
+                  <p className="fs-m ln-m fw-400 color-grey400">
+                    <span className="fw-700 color-grey700">
+                      {artical.textTopic}
+                    </span>
+                    {artical.textTime}
+                  </p>
+                  <h3 className="fs-l ln-xl fw-700 color-grey700 my-xs">
+                    {artical.title}
+                  </h3>
+                  <p className="fs-m ln-m fw-400 color-grey400 mb-s">
+                    {artical.description}
+                  </p>
+                  <ButtonPress
+                    variant={"outline-primary"}
+                    value={"Read More"}
+                  />
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
         </Carousel>
       </Container>
     </section>

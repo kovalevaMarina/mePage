38cfb8f90b43198a5ec66149a7125f97f1737a9e
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-08-10 21:01:13 +0300

    Add media styles for competitors section

--- src/components/Competitors/Competitors.js
+++ src/components/Competitors/Competitors.js
@@ -1,129 +1,19 @@
 import "./Competitors.scss";
-import circleCheck from "../../assets/icons/check-circle.svg";
-import circleX from "../../assets/icons/x-circle.svg";
-import Logo from "../Logo/Logo";
 import ButtonPress from "../Button/Button";
+import CompetitorsCard from "./CompetitorsCard";
 
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
-import Table from "react-bootstrap/Table";
 
 function Competitors() {
   return (
-    <section className="competitors mb-s">
+    <section className="competitors pb-s pb-lg-5">
       <Container>
-        <h3 className="title-h3 mb-2">Competitors</h3>
-        <h2 className="title-h2">Why ME Page?</h2>
-        <Card>
-          <Card.Body>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Features</th>
-                  <th className="table-cell_rigth">
-                    <Logo
-                      width={86}
-                      height={28}
-                      firstColor={"#3A83F6"}
-                      secondColor={"#3E4857"}
-                    />
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Share Project</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleCheck} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Customized Templates</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleCheck} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Customized Ready-made Blocks</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleCheck} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>HTML and CSS editing</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleCheck} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Advanced Mode</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleCheck} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Average Price</td>
-                  <td className="table-cell_rigth">$10</td>
-                </tr>
-                <tr>
-                  <td>Customer Support</td>
-                  <td className="table-cell_rigth">24/7 live chat and email</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Features</th>
-                  <th className="table-cell_rigth">Other Constructors</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Share Project</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleX} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Customized Templates</td>
-                  <td className="table-cell_rigth">Limited</td>
-                </tr>
-                <tr>
-                  <td>Customized Ready-made Blocks</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleX} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>HTML and CSS editing</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleX} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Advanced Mode</td>
-                  <td className="table-cell_rigth">
-                    <img src={circleX} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Average Price</td>
-                  <td className="table-cell_rigth">$20</td>
-                </tr>
-                <tr>
-                  <td>Customer Support</td>
-                  <td className="table-cell_rigth">Email only</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card.Body>
-        </Card>
-        <p className="fs-s fw-400 ln-2s color-grey400 text-center my-s">
+        <h3 className="title-h3 mb-2 text-uppercase text-center">
+          Competitors
+        </h3>
+        <h2 className="title-h2 h1 text-center">Why ME Page?</h2>
+        <CompetitorsCard />
+        <p className="competitors-text fs-s fw-400 ln-2s color-grey400 text-center my-s">
           As seen in the competition, ME Page offers several features that are
           comparable to other web-constructors, customizable templates & blocks,
           ability to share project for view or edit, ability to edit HTML and
---
+++ src/components/Competitors/CompetitorsCard.js
@@ -0,0 +1,175 @@
+import "./CompetitorsCard.scss";
+import circleCheck from "../../assets/icons/check-circle.svg";
+import circleX from "../../assets/icons/x-circle.svg";
+import Logo from "../Logo/Logo";
+
+import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function CompetitorsCard() {
+  return (
+    <Row className="d-flex flex-column flex-lg-row gap-4 gap-lg-0 py-4">
+      <Col lg={4}>
+        <Card className="d-none d-lg-flex">
+          <Card.Body>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Features</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Share Project</td>
+                </tr>
+                <tr>
+                  <td>Customized Templates</td>
+                </tr>
+                <tr>
+                  <td>Customized Ready-made Blocks</td>
+                </tr>
+                <tr>
+                  <td>HTML and CSS editing</td>
+                </tr>
+                <tr>
+                  <td>Advanced Mode</td>
+                </tr>
+                <tr>
+                  <td>Average Price</td>
+                </tr>
+                <tr>
+                  <td>Customer Support</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col lg={4}>
+        <Card>
+          <Card.Body>
+            <Table>
+              <thead>
+                <tr>
+                  <th className="d-lg-none">Features</th>
+                  <th className="align-middle text-end text-lg-center">
+                    <Logo
+                      width={86}
+                      height={25}
+                      firstColor={"#3A83F6"}
+                      secondColor={"#3E4857"}
+                    />
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="d-lg-none">Share Project</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleCheck} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customized Templates</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleCheck} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customized Ready-made Blocks</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleCheck} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">HTML and CSS editing</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleCheck} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Advanced Mode</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleCheck} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Average Price</td>
+                  <td className="text-end text-lg-center">$10</td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customer Support</td>
+                  <td className="text-end text-lg-center">
+                    24/7 live chat and email
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col lg={4}>
+        <Card>
+          <Card.Body>
+            <Table>
+              <thead>
+                <tr>
+                  <th className="d-lg-none">Features</th>
+                  <th className="align-middle text-end text-lg-center">
+                    Other Constructors
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="d-lg-none">Share Project</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleX} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customized Templates</td>
+                  <td className="align-middle text-end text-lg-center">
+                    Limited
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customized Ready-made Blocks</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleX} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">HTML and CSS editing</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleX} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Advanced Mode</td>
+                  <td className="text-end text-lg-center">
+                    <img src={circleX} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Average Price</td>
+                  <td className="align-middle text-end text-lg-center">$20</td>
+                </tr>
+                <tr>
+                  <td className="d-lg-none">Customer Support</td>
+                  <td className="align-middle text-end text-lg-center">
+                    Email only
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
+export default CompetitorsCard;

885f97afc6ff9b1c0d25425ca772ec5a520985c8
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-08-10 19:21:52 +0300

    Fix images for hero section and styles main title

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,7 @@
 import "./Hero.scss";
 import mainImgMob from "../../assets/images/main-img-mob.png";
+import mainImgDesktop from "../../assets/images/main-img-desktop.png";
+
 import ButtonPress from "../Button/Button";
 import Container from "react-bootstrap/Container";
 
@@ -9,15 +11,29 @@ function Hero() {
       <Container>
         <div className="d-flex flex-column flex-lg-column-reverse gap-lg-5">
           <div className="hero-img mb-l">
-            {/* TODO add another img in lg, how? */}
-            <img src={mainImgMob} alt="Screen picture" />
+            <img
+              className="hero-img_mobile d-block d-lg-none"
+              src={mainImgMob}
+              alt="Screen for hero section"
+            />
+            <img
+              className="hero-img_desktop d-none d-lg-block"
+              src={mainImgDesktop}
+              alt="Screen for hero section"
+            />
           </div>
           <div>
             <h1 className="display-5 fw-700 text-center color-grey700">
               Flexible Constructor for Creating
             </h1>
-            <div className="text-center color-blue display-5 fm-arial fw-700">
-              Perfect Websites
+            <div className="text-center color-blue display-5 fm-arial fw-700 my-2">
+              <span className="hero-title p-2">
+                Perfect Websites
+                <span className="square"></span>
+                <span className="square2"></span>
+                <span className="square3"></span>
+                <span className="square4"></span>
+              </span>
             </div>
             <p className="hero-text mx-auto mb-0 pt-xs pb-l color-grey400 h6 ln-m text-center">
               Build a website that fits your needs, not the other way around

223f1aa65cb4513bb6c3510474bb9e4e791b6635
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-08-11 19:03:04 +0300

    Add media styles for articles section

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -12,59 +12,64 @@ import articalThird from "../../assets/images/article-third.png";
 import ButtonPress from "../Button/Button";
 
 function Articles() {
-  const articalsData = [
-    {
-      imgUrl: articalFirst,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "Your Dream Website with a User-Friendly Constructor",
-      description:
-        "Building a website can be a daunting task, especially for those who lack technical expertise.",
-    },
+  const articlesData = [
+    [
+      {
+        imgUrl: articalFirst,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "Your Dream Website with a User-Friendly Constructor",
+        description:
+          "Building a website can be a daunting task, especially for those who lack technical expertise.",
+      },
 
-    {
-      imgUrl: articalsecond,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "How ME-Page Can Help You Build Your Business Online",
-      description:
-        "With customizable templates, user-friendly interfaces, and integrated e-commerce features...",
-    },
+      {
+        imgUrl: articalsecond,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "How ME-Page Can Help You Build Your Business Online",
+        description:
+          "With customizable templates, user-friendly interfaces, and integrated e-commerce features...",
+      },
 
-    {
-      imgUrl: articalThird,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "Maximizing the Potential of Online Store with ME-Page",
-      description:
-        "From creating a visually appealing website to integrating essential features such as social...",
-    },
-    {
-      imgUrl: articalFirst,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "Your Dream Website with a User-Friendly Constructor",
-      description:
-        "Building a website can be a daunting task, especially for those who lack technical expertise.",
-    },
+      {
+        imgUrl: articalThird,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "Maximizing the Potential of Online Store with ME-Page",
+        description:
+          "From creating a visually appealing website to integrating essential features such as social networks...",
+      },
+    ],
 
-    {
-      imgUrl: articalsecond,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "How ME-Page Can Help You Build Your Business Online",
-      description:
-        "With customizable templates, user-friendly interfaces, and integrated e-commerce features...",
-    },
+    [
+      {
+        imgUrl: articalFirst,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "Your Dream Website with a User-Friendly Constructor",
+        description:
+          "Building a website can be a daunting task, especially for those who lack technical expertise.",
+      },
 
-    {
-      imgUrl: articalThird,
-      textTopic: "Tips, Constructor",
-      textTime: " - March 10, 2023",
-      title: "Maximizing the Potential of Online Store with ME-Page",
-      description:
-        "From creating a visually appealing website to integrating essential features such as social...",
-    },
+      {
+        imgUrl: articalsecond,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "How ME-Page Can Help You Build Your Business Online",
+        description:
+          "With customizable templates, user-friendly interfaces, and integrated e-commerce features...",
+      },
+
+      {
+        imgUrl: articalThird,
+        textTopic: "Tips, Constructor",
+        textTime: " - March 10, 2023",
+        title: "Maximizing the Potential of Online Store with ME-Page",
+        description:
+          "From creating a visually appealing website to integrating essential features such as social networks...",
+      },
+    ],
   ];
 
   return (
@@ -74,41 +79,78 @@ function Articles() {
         <h2 className="title-h2 fs-1 text-center">
           Latest news from ME-Page team
         </h2>
-        <Row>
-          <Carousel>
-            {articalsData.map((artical) => {
-              return (
-                <Col xs={12} lg={4}>
-                  <Carousel.Item>
-                    <img
-                      src={artical.imgUrl}
-                      className="d-block w-100"
-                      alt={`Artical about ${artical.title}`}
+        <Carousel className="d-lg-none">
+          {articlesData.flat().map((artical) => {
+            return (
+              <Carousel.Item>
+                <div>
+                  <img
+                    src={artical.imgUrl}
+                    className="d-block w-100"
+                    alt={`Artical about ${artical.title}`}
+                  />
+                  <Carousel.Caption>
+                    <p className="fs-m ln-m fw-400 color-grey400">
+                      <span className="fw-700 color-grey700">
+                        {artical.textTopic}
+                      </span>
+                      {artical.textTime}
+                    </p>
+                    <h3 className="fs-l ln-xl fw-700 color-grey700 my-xs">
+                      {artical.title}
+                    </h3>
+                    <p className="fs-m ln-m fw-400 color-grey400 mb-s">
+                      {artical.description}
+                    </p>
+                    <ButtonPress
+                      variant={"outline-primary"}
+                      value={"Read More"}
                     />
-                    <Carousel.Caption>
-                      <p className="fs-m ln-m fw-400 color-grey400">
-                        <span className="fw-700 color-grey700">
-                          {artical.textTopic}
-                        </span>
-                        {artical.textTime}
-                      </p>
-                      <h3 className="fs-l ln-xl fw-700 color-grey700 my-xs">
-                        {artical.title}
-                      </h3>
-                      <p className="fs-m ln-m fw-400 color-grey400 mb-s">
-                        {artical.description}
-                      </p>
-                      <ButtonPress
-                        variant={"outline-primary"}
-                        value={"Read More"}
-                      />
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                </Col>
-              );
-            })}
-          </Carousel>
-        </Row>
+                  </Carousel.Caption>
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+        <Carousel className="d-none d-lg-block">
+          {articlesData.map((item) => {
+            return (
+              <Carousel.Item>
+                <Row>
+                  {item.map((article) => {
+                    return (
+                      <Col lg={4}>
+                        <img
+                          src={article.imgUrl}
+                          className="d-block w-100"
+                          alt={`Artical about ${article.title}`}
+                        />
+                        <Carousel.Caption>
+                          <p className="fs-m ln-m fw-400 color-grey400">
+                            <span className="fw-700 color-grey700">
+                              {article.textTopic}
+                            </span>
+                            {article.textTime}
+                          </p>
+                          <h3 className="fs-l ln-xl fw-700 color-grey700 my-xs">
+                            {article.title}
+                          </h3>
+                          <p className="fs-m ln-m fw-400 color-grey400 mb-s">
+                            {article.description}
+                          </p>
+                          <ButtonPress
+                            variant={"outline-primary"}
+                            value={"Read More"}
+                          />
+                        </Carousel.Caption>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </Container>
     </section>
   );
